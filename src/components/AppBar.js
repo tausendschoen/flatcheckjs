01ec81986ae6f50d8bdad4d5d4c2fafffe7a4da7
Michael <michael.carell@web.de>
2023-11-01 16:52:47 +0100

    AppBar Menu schließen wenn Dialog angezeigt wird.

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -51,8 +51,8 @@ export default function ButtonAppBar(props) {
                             'aria-labelledby': 'basic-button',
                         }}
                     >
-                        <MenuItem onClick={(e) => setHelpDialog(true)}>Hilfe</MenuItem>
-                        <MenuItem onClick={(e) => setAboutDialog(true)}>Über diese App</MenuItem>
+                        <MenuItem onClick={(e) => {setOpen(false); setHelpDialog(true) }}>Hilfe</MenuItem>
+                        <MenuItem onClick={(e) => {setOpen(false); setAboutDialog(true)}}>Über diese App</MenuItem>
                     </Menu>
 
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
--- src/components/appVersion.js
+++ src/components/appVersion.js
@@ -1,1 +1,1 @@
-export const AppVersion = "0.1.14";
+export const AppVersion = "0.1.15";
